d512282adb9304668c3d8c47e66d237a9e1c472f
merwin <merwingeee@@gmail.com>
2020-09-15 22:16:17 +0200

    Changed Companion app directory

---
+++ Companion/app/src/main/java/com/example/gofindhololenscompanion/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.gofindhololenscompanion;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
