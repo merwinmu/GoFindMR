31a82dad2aad51e4afb44df804490c24594db577
merwin <merwingeee@@gmail.com>
2020-09-19 00:51:38 +0200

    Added method to get GPS Coordinates

--- Companion/app/src/main/java/com/example/gofindhololenscompanion/MainActivity.java
+++ Companion/app/src/main/java/com/example/gofindhololenscompanion/MainActivity.java
@@ -1,13 +1,92 @@
 package com.example.gofindhololenscompanion;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE_LOCATION_PERMISSION = 1;
+    TextView latlong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        latlong = (TextView) findViewById(R.id.latlong);
+
+
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) { // Check permisson to use GPS called from Async Thread
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE_LOCATION_PERMISSION);
+        }
+
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter(); // getting permission to use Bluetooth
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+        } else if (!mBluetoothAdapter.isEnabled()) {
+            // Bluetooth is not enabled :)
+            Toast.makeText(MainActivity.this, "PLEASE ENABLE BLUETOOTH", Toast.LENGTH_LONG).show();
+            startActivityForResult(new Intent(android.provider.Settings.ACTION_SETTINGS), 0);
+        }
+        getCurrentLocation();
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) { // Again asking for permission and getting location
+        if (requestCode == REQUEST_CODE_LOCATION_PERMISSION) {
+            if (!Arrays.asList(grantResults).contains(PackageManager.PERMISSION_DENIED)){
+                getCurrentLocation();
+            }
+        }
+    }
+
+    private void getCurrentLocation() {
+        LocationRequest locrequest = new LocationRequest();
+        locrequest.setInterval(1000);
+        locrequest.setFastestInterval(3000);
+        locrequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        LocationServices.getFusedLocationProviderClient(MainActivity.this).requestLocationUpdates(locrequest, new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                LocationServices.getFusedLocationProviderClient(MainActivity.this).removeLocationUpdates(this);
+                if (locationResult != null && locationResult.getLocations().size() > 0) {
+                    int latestlocationIndex = locationResult.getLocations().size() - 1;
+                    double latitude = locationResult.getLocations().get(latestlocationIndex).getLatitude();
+                    double longitude = locationResult.getLocations().get(latestlocationIndex).getLongitude();
+
+                    Log.d("GPS",String.valueOf(latitude) + String.valueOf(longitude));
+                    latlong.setText( String.format("Latitude: %s\nLongitude: %s", latitude, longitude));
+                }
+            }
+
+        }, Looper.getMainLooper());
+
     }
 }
