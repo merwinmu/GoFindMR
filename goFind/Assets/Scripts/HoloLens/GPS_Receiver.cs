929cbf539e788d793aa743727008db5e54c8827c
Merwin <m.mudavamkunnel@unibas.ch>
2020-09-24 00:20:38 +0200

    Added a BLE Watcher for HoloLens2 (needs testing)

--- goFind/Assets/Scripts/HoloLens/GPS_Receiver.cs
+++ goFind/Assets/Scripts/HoloLens/GPS_Receiver.cs
@@ -1,62 +1,39 @@
-﻿using System;
-using HoloLens;
-using TMPro;
-using UnityEngine; 
-#if NETFX_CORE 
+﻿using UnityEngine;
+#if NETFX_CORE
 using Windows.Devices.Bluetooth.Advertisement;
 using System.Runtime.InteropServices.WindowsRuntime;
 #endif
 
 public class GPS_Receiver : MonoBehaviour
 {
-    TextMeshPro textMeshPro;
-    public static double latitude;
-
-    
 #if NETFX_CORE
-BluetoothLEAdvertisementWatcher watcher;
-public static ushort BEACON_ID = 24;
+    BluetoothLEAdvertisementWatcher watcher;
+    public static ushort BEACON_ID = 1775;
 #endif
-
-    private EventProcessor eventProcessor;
-
-    private void Start()
-    {
-        latitude = 0.2;
-    }
 
     void Awake()
     {
-        textMeshPro = GetComponent<TextMeshPro>();
-       
         
-        eventProcessor = (EventProcessor)FindObjectOfType(typeof(EventProcessor));
-
 #if NETFX_CORE
-watcher = new BluetoothLEAdvertisementWatcher();
-var manufacturerData = new BluetoothLEManufacturerData {CompanyId = BEACON_ID};
-watcher.AdvertisementFilter.Advertisement.ManufacturerData.Add(manufacturerData);
-watcher.Received += Watcher_Received;
-watcher.Start();
-#endif
-
-#if NETFX_CORE
-        private async void Watcher_Received(BluetoothLEAdvertisementWatcher sender,
-            BluetoothLEAdvertisementReceivedEventArgs args)
+        watcher = new BluetoothLEAdvertisementWatcher();
+        var manufacturerData = new BluetoothLEManufacturerData
         {
-            ushort identifier = args.Advertisement.ManufacturerData[0].CompanyId;
-            byte[] data = args.Advertisement.ManufacturerData[0].Data.ToArray();
-            eventProcessor.QueueEvent(() => { });
-            latitude = BitConverter.ToDouble(data, 0);
-           
-            
-        }
+        CompanyId = BEACON_ID
+        };
+        watcher.AdvertisementFilter.Advertisement.ManufacturerData.Add(manufacturerData);
+        watcher.Received += Watcher_Received;
+        watcher.Start();
 #endif
     }
-    
-    void Update()
+#if NETFX_CORE
+    private async void Watcher_Received(BluetoothLEAdvertisementWatcher sender, BluetoothLEAdvertisementReceivedEventArgs args)
     {
-        textMeshPro.text = latitude.ToString();
-
+        ushort identifier = args.Advertisement.ManufacturerData[0].CompanyId;
+        byte[] data = args.Advertisement.ManufacturerData[0].Data.ToArray();
+        // Updates to Unity UI don't seem to work nicely from this callback so just store a reference to the data for later processing.
+        Debug.Log(data.ToString);
     }
+#endif
+    private byte[] data;
+    
 }
