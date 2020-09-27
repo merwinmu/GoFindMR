2cc0adb4af4024bec5356289f85d9e81eb8f37ed
Merwin <m.mudavamkunnel@unibas.ch>
2020-09-27 15:14:43 +0200

    Added eg. Picture from Server
    - Added follow me menu

--- goFind/Android/LocationAwarenessPlugin/las/src/main/java/ch/unibas/dmi/dbis/las/annotation/UnityCallable.java
+++
@@ -1,9 +0,0 @@
-package ch.unibas.dmi.dbis.las.annotation;
-
-/**
- * A simple flag-annotation to indicate that a method is called via Unity's AndroidJavaObject
- * Created by Loris on 19.06.2017.
- */
-
-public @interface UnityCallable {
-}
---
+++ gofind-master/Assets/HoloLens/Scripts/ImageLoader.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageLoader : MonoBehaviour
+{
+    public string URL =
+        "https://www.wallpaperup.com/uploads/wallpapers/2014/05/02/348237/418634e46415dc2424a305da3a77cd1b.jpg";
+
+    public Renderer thisRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadFromLikeCoroutine());
+        
+        thisRenderer.material.color = Color.red;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator LoadFromLikeCoroutine()
+    {
+     Debug.Log("Loading...");   
+     WWW wwwLoader = new WWW(URL);
+     yield return wwwLoader;
+     
+     Debug.Log("Loaded");
+     thisRenderer.material.color = Color.white;
+     thisRenderer.material.mainTexture = wwwLoader.texture;
+    }
+}
